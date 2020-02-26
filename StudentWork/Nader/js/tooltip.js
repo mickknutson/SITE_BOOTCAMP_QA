8e3088d2baa9f7cdbdd96cc4fb366987a7e25062
Nader <na511dr@hotmail.com>
2020-02-26 10:24:43 +0300

    update tooltip.js to include the navigator func

--- StudentWork/Nader/js/tooltip.js
+++ StudentWork/Nader/js/tooltip.js
@@ -1,1 +1,8 @@
-document.createElement("I am the Hero")
+//document.createElement("I am the Hero")
+
+function name() {
+    var appName = navigator.appVersion
+    console.log(appName);
+}
+
+name();
