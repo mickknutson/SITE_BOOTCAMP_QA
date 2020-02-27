353240018c291e6efe187be48144386f9fbc8049
Abdullah Alshehri <alshehri.a.j@gmail.com>
2020-02-27 12:47:56 +0300

    started core java project

---
+++ StudentWork/abdullah_j/Java/coreJava/src/com/site/core/Main.java
@@ -0,0 +1,13 @@
+package com.site.core;
+
+public class Main {
+    
+        private String anInstance;
+
+        public static void main(String[] args) {
+
+            String aLocalVariable;
+            System.out.println("Hello Abdullah");
+
+        } // end main()
+    } // The end
