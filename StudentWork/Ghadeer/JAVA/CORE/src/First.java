6db3cb3704903f02275bd4e2c9e540e4144127d7
ghadeer <ghadeer.beniedar@gmail.com>
2020-02-27 12:01:16 +0300

    created java folder.

---
+++ StudentWork/Ghadeer/JAVA/CORE/src/First.java
@@ -0,0 +1,6 @@
+public class First {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("ghadeer");
+    }
+}
---
+++ StudentWork/Ghadeer/JAVA/CORE/src/com/basiclogic/core/Main.java
@@ -0,0 +1,9 @@
+package com.basiclogic.core;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("ghadeer");
+    }
+}
