e274fa6b0e6714249c50f59f8805063676c7544d
Abdullah Alshehri <alshehri.a.j@gmail.com>
2020-02-27 15:11:30 +0300

    Added basic method

--- StudentWork/abdullah_j/Java/coreJava/src/com/site/core/Main.java
+++ StudentWork/abdullah_j/Java/coreJava/src/com/site/core/Main.java
@@ -1,13 +1,29 @@
 package com.site.core;
 
 public class Main {
-    
-        private String anInstance;
 
-        public static void main(String[] args) {
+    private String anInstance;
 
-            String aLocalVariable;
-            System.out.println("Hello Abdullah");
+    public static void main(String[] args) {
 
-        } // end main()
-    } // The end
+        String aLocalVariable;
+        System.out.println("Hello Abdullah");
+
+        //basic();
+
+
+    } // end main()
+
+    private static void basic() {
+        int number = 1;
+
+        String output = (number % 2 == 0) ? "even" : "odd";
+        System.out.println(output);
+
+        System.out.println(number);
+        System.out.println(++number);
+        System.out.println(number++);
+        System.out.println(number--);
+        System.out.println(--number);
+    }
+} // The end
