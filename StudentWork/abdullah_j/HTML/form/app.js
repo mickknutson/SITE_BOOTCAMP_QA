caef2cd19be773225c96f5e8ae392c9f138ad12d
Abdullah Alshehri <alshehri.a.j@gmail.com>
2020-02-27 10:33:39 +0300

    added form page

---
+++ StudentWork/abdullah_j/HTML/form/app.js
@@ -0,0 +1,17 @@
+
+
+let fName = document.querySelector('#firstName');
+let lName = document.querySelector('#lastName');
+
+let email = document.querySelector('#email');
+let pwd = document.querySelector('#pwd');
+
+
+function show(){
+
+    document.write('First anme: ' + fName.value + '<br>' );
+    document.write('Last anme: ' + lName.value + '<br>');
+    document.write('email: ' + email.value + '<br>');
+    document.write('password: ' + pwd.value + '<br>');
+
+}
