d3f3b52d68883f29cb602050f8570dcd30fe3de9
ghadeer <ghadeer.beniedar@gmail.com>
2020-02-27 09:49:25 +0300

    added JS file

---
+++ StudentWork/Ghadeer/assignment#1.js
@@ -0,0 +1,10 @@
+<script>
+var fname=document.getElementById("fname");
+var lname=document.getElementById("lname");
+var email=document.getElementById("Email");
+var pass=document.getElementById("Password");
+alert('User's full name: '+fname+lname+'\nUser's Email: '+email+'\nUser's password: '+pass);
+
+
+
+</script>
