0bd47ce8844e7a9b0262193a54a82adc7d44d9ef
ghadeer <ghadeer.beniedar@gmail.com>
2020-02-27 15:46:23 +0300

    added java practice file

--- StudentWork/Ghadeer/JAVA/CORE/src/First.java
+++ StudentWork/Ghadeer/JAVA/CORE/src/First.java
@@ -1,6 +1,16 @@
 public class First {
     public static void main(String[] args) {
         // write your code here
-        System.out.println("ghadeer");
+int num=2;
+String output=(num%2==0)?"even number":"odd number";
+        basic(num, output);
+
+    }
+
+    private static void basic(int num, String output) {
+        System.out.println(output);
+        System.out.println(++num);
+        System.out.println(num++);
+        System.out.println(num);
     }
 }
