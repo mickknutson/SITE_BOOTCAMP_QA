61d83f8be4eff32161812c68080373abe234cb13
Mick Knutson <mickknutson@gmail.com>
2020-03-01 09:26:13 +0300

    SITE - updating Animal

---
+++ StudentWork/mick/Java/core/src/com/baselogic/core/animal/Cat.java
@@ -0,0 +1,9 @@
+package com.baselogic.core.animal;
+
+public class Cat extends Animal{
+
+    public void talk(){
+        System.out.println("meow");
+    }
+
+}
---
+++ StudentWork/mick/Java/core/src/com/baselogic/core/fruit/Fruit.java
@@ -0,0 +1,18 @@
+package com.baselogic.core.fruit;
+
+public class Fruit {
+
+    // Encapsulate:
+    private String foo = "FooBar";
+
+    // Mutator (Setter)
+    void setFoo(String foo){
+        this.foo = foo;
+    }
+
+    // Accessor (Getter)
+    String getFoo(){
+        return this.foo;
+    }
+
+}
