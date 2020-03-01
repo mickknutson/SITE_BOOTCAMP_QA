68d207b7d936a87ecb19842475b9c2f46ff5864c
Monerah Balhareth <sweet-roz2010@hotmail.com>
2020-03-01 15:30:54 +0300

    Strings and Arrays

--- StudentWork/MONERAHS/core/src/com/company/core/StringTest.java
+++ StudentWork/MONERAHS/core/src/com/company/core/StringTest.java
@@ -1,4 +1,16 @@
 package com.company.core;
 
 public class StringTest {
+
+    public static void main(String[] args) {
+        String s1 = "java";
+        char ch[] = {'s', 't', 'r', 'i', 'n', 'g', 's'};
+        String s2 = new String(ch);//converting char array to string  
+        String s3 = new String("examples"); //creating java string by new keyword  
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+
+    }
 }
