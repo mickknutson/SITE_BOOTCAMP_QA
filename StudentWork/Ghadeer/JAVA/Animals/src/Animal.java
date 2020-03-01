5a344acff5b91e5bdb24e11567ef9549bab3c99b
ghadeer <ghadeer.beniedar@gmail.com>
2020-03-01 09:58:22 +0300

    animal example file updated

--- StudentWork/Ghadeer/JAVA/Animals/src/Animal.java
+++ StudentWork/Ghadeer/JAVA/Animals/src/Animal.java
@@ -1,22 +1,36 @@
-
-
 public class Animal {
-    
-    public void eat(){
-        System.out.println("nom.. nommm.. nomm");
-
+    private String name;
+    private static String label;
+    //Default constructor
+    Animal(){
+        this.name="Default";
     }
-    public static void main(String[]args){
-        Animal animal;
-        animal = new Animal();
+    //receiving data constructor
+    Animal(String name){
+        this.name=name;
+    }
+    public void eat(){
+        System.out.println(this.name+"  nom.. nommm.. nomm");
+    }
 
-        animal.eat();
-       Animal dog=new Dog();
+    //Main method
+    public static void main(String[]args){
+        Animal animal1;
+        animal1 = new Animal();
+        Animal animal2;
+        animal2 = new Animal("Bear");
+
+        //Examining the two different type of constructors
+        animal1.eat();
+        animal2.eat();
+
+        //extends objects
+
+        Animal dog=new Dog();
         Animal cat=new Cat();
         ((Dog)dog).talk();
         ((Cat)cat).talk();
         dog.eat();
-
-
+       
     }
 }
