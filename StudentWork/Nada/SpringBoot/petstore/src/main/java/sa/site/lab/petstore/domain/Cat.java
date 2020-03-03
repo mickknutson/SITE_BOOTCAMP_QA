5d8daf9ffaacece10b016683efddf271972da8aa
Nada <nhalghani@gmail.com>
2020-03-03 15:58:43 +0300

    Initial petstore commit

---
+++ StudentWork/Nada/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/domain/Cat.java
@@ -0,0 +1,21 @@
+package sa.site.lab.petstore.domain;
+
+
+public class Cat implements Animal {
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void talk(String voice) {
+        System.out.println(voice);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("sip, sip, sip");
+    }
+
+}
