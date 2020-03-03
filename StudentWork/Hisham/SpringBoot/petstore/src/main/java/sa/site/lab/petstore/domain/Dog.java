815cefc38ff0546798e1a5ece78a124310d3196e
Hisham Bin Seddeq <prog.hbs@hotmail.com>
2020-03-03 11:43:01 +0300

    SpringBoot Added

---
+++ StudentWork/Hisham/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/domain/Dog.java
@@ -0,0 +1,25 @@
+package sa.site.lab.petstore.domain;
+
+public class Dog implements Animal
+{
+
+    private String name;
+
+    public Dog(String name)
+    {
+        this.name = name;
+
+    }
+
+    @Override
+    public void eat()
+    {
+        System.out.println("Nom , nom , nom");
+    }
+
+    public void talk(String voice)
+    {
+        System.out.println(voice);
+    }
+
+}
