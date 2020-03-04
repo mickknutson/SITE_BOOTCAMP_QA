4b829c6dc33acbb36a13bc4efe9640cacd6316ff
Lama <lamafa97@gmail.com>
2020-03-04 10:35:50 +0300

    Spring boot updates

--- StudentWork/Lama Alyousef/SpringBoots/petstore/src/main/java/sa/site/lab/petstore/dao/AnimalDaoImpl.java
+++ StudentWork/Lama Alyousef/SpringBoots/petstore/src/main/java/sa/site/lab/petstore/dao/AnimalDaoImpl.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Repository;
 import sa.site.lab.petstore.domain.Animal;
 import sa.site.lab.petstore.domain.Cat;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /*
  * Data Access for Pet Domain Object
  *
@@ -20,9 +25,32 @@ import sa.site.lab.petstore.domain.Cat;
 
 @Repository
 public class AnimalDaoImpl implements AnimalDao {
+        // LAP: Creat Map of Animals.
+     Map <Integer,Animal> animals = new HashMap<>();
 
-    public Animal findPet(int id) {
-        return new Cat("Bella");
+    public AnimalDaoImpl(){
+        // LAB: Populate Map with 3+ Animals.
+        animals.put(1, new Cat("Bella"));
+        animals.put(2, new Cat("Kitty"));
+        animals.put(2, new Cat("Katy"));
     }
 
+
+    public Animal findPet(int id) {
+        System.out.println("* AnimalDao: " + id);
+
+        //animal.containsKey(id)
+        return animals.get(id);
+    }
+
+    public List<Animal> findAll(){
+        System.out.println(" * AnimalDao.findAll()");
+        // Create a List of Animals
+        // LAB: return List
+
+        // LAB: return List<Animal> from Map
+        ArrayList<Animal> valueList = new ArrayList<Animal>(animals.values());
+
+        return valueList;
+    }
 }
