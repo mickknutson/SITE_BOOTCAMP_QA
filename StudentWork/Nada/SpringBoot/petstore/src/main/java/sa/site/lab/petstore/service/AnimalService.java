cbc0505dac0d52bcffa8b6a07cc5f25a80c4dabc
Nada <nhalghani@gmail.com>
2020-03-04 10:51:33 +0300

    SpringBoot- Petstore project updated..

--- StudentWork/Nada/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/service/AnimalService.java
+++ StudentWork/Nada/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/service/AnimalService.java
@@ -4,9 +4,12 @@ import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import sa.site.lab.petstore.domain.Animal;
 
+import java.util.List;
+
 @Repository // stereotype
 public interface AnimalService {
 
     public Animal findPet(int id);
 
+    List<Animal> findAll();
 }
