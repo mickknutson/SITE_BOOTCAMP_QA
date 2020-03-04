2071668f6ab2cc04d94fc4daec1b52d8aeba99e3
Bashaer Jamil <bashaerjamil@gmail.com>
2020-03-04 15:15:54 +0300

    Implementing MVN

--- StudentWork/Besh/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/dao/AnimalDaoDatabaseImpl.java
+++ StudentWork/Besh/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/dao/AnimalDaoDatabaseImpl.java
@@ -7,13 +7,18 @@ import java.util.List;
 
 @Repository
 public class AnimalDaoDatabaseImpl implements AnimalDao{
-    public Animal findPet(int id){
+    public Animal findByID(int id){
         System.out.println("AnimalDaoDatabaseImpl.findPet");
         throw new RuntimeException("Not Available Yet");
     }
     @Override
     public List<Animal> findAll(){
         System.out.println("AnimalDaoDatabaseImpl.findAll");
+        throw new RuntimeException("Not Available Yet");
+    }
+    @Override
+    public void add (Animal animal){
+        System.out.println("AnimalDaoDatabaseImpl.add");
         throw new RuntimeException("Not Available Yet");
     }
 }
