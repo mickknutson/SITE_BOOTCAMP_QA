2b3793f21bf5635b966e09fdbabe30c265543b79
Hisham Bin Seddeq <prog.hbs@hotmail.com>
2020-03-04 10:02:26 +0300

    changed allanimals array into a map

--- StudentWork/Hisham/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/dao/AnimalDaoImpl.java
+++ StudentWork/Hisham/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/dao/AnimalDaoImpl.java
@@ -7,31 +7,56 @@ import sa.site.lab.petstore.domain.Cat;
 import sa.site.lab.petstore.domain.Dog;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class AnimalDaoImpl implements AnimalDao
 {
+    Map<Integer,Animal> allAnimals = new HashMap<>();
+
+    public AnimalDaoImpl()
+    {
+        allAnimals.put(1,new Cat("kitten"));
+        allAnimals.put(2,new Cat("Cimba"));
+        allAnimals.put(3,new Dog("Foo"));
+        allAnimals.put(4,new Dog("Bar"));
+
+    }
 
     @Override
     public Animal findPet(int id)
     {
-        return new Cat("Kitty");
+        if(allAnimals.containsKey(id))
+        {
+            System.out.println("### AnimalDao : " + allAnimals.get(id).getName());
+            return allAnimals.get(id);
+        }
+        else
+        {
+            System.out.println("No Animal found with this id");
+            return null;
+        }
     }
 
     @Override
     public List<Animal> findAll()
     {
-        List<Animal> allAnimals = new ArrayList<>();
-
-        Cat cat1 = new Cat("Kitten");
-        Cat cat2 = new Cat("foo");
-        Dog dog1 = new Dog("bar");
-        allAnimals.add(cat1);
-        allAnimals.add(cat2);
-        allAnimals.add(dog1);
 
 
-        return allAnimals;
+
+
+//        List<Animal> allAnimals = new ArrayList<>();
+//
+//        Cat cat1 = new Cat("Kitten");
+//        Cat cat2 = new Cat("foo");
+//        Dog dog1 = new Dog("bar");
+//        allAnimals.add(cat1);
+//        allAnimals.add(cat2);
+//        allAnimals.add(dog1);
+
+
+        return new ArrayList<Animal>(allAnimals.values());
     }
 }
