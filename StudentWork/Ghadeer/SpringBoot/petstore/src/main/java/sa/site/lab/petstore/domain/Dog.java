803d56c9473fd4bc5e50ff89165de8fcb7caccd1
ghadeer <ghadeer.beniedar@gmail.com>
2020-03-04 16:31:08 +0300

    updates

--- StudentWork/Ghadeer/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/domain/Dog.java
+++ StudentWork/Ghadeer/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/domain/Dog.java
@@ -6,26 +6,34 @@ import javax.persistence.Entity;
 @Entity
 public class Dog implements Animal {
     private String name;
-    Dog (){
+    //map fields to DB columns
+    private int id;
+
+    Dog() {
         setName("puppy");
     }
-    public Dog(String name){
+
+    public Dog(String name) {
         setName(name);
     }
+
     @Override
-    public void talk(){
+    public void talk() {
         System.out.println("park,park,park,park,..... ");
     }
+
     @Override
-    public void eat(){
-        System.out.println(getName()+" yummm");
+    public void eat() {
+        System.out.println(getName() + " yummm");
 
     }
-        //accessor
-        public String getName(){
+
+    //accessor
+    public String getName() {
         return this.name;
     }
-    public void setName(String name){
-        this.name=name;
+
+    public void setName(String name) {
+        this.name = name;
     }
-}
+}//the end...
