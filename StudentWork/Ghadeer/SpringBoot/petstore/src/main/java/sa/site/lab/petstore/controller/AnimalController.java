16417ac0c11f4cd14e846080f07e0292d433385a
ghadeer <ghadeer.beniedar@gmail.com>
2020-03-04 09:47:35 +0300

    Lab Assignment added

--- StudentWork/Ghadeer/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/controller/AnimalController.java
+++ StudentWork/Ghadeer/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/controller/AnimalController.java
@@ -1,10 +1,16 @@
 package sa.site.lab.petstore.controller;
 
+import org.w3c.dom.ls.LSOutput;
 import sa.site.lab.petstore.domain.Animal;
 
 import java.util.List;
+import java.util.Map;
 
 public interface AnimalController {
-    public Animal findPet(int id);
-    List<Animal> findAll();
+
+     Animal findPet(int id);
+
+    Map findAll();
+
+//    void fillMethod();
 }
