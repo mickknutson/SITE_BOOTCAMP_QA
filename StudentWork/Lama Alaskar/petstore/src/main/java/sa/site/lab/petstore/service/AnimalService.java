7e9fccc8edc69df8b668b9d12afb358e39db4c14
Lama <lama3skr@gmail.com>
2020-03-04 08:53:57 +0300

    update

---
+++ StudentWork/Lama Alaskar/petstore/src/main/java/sa/site/lab/petstore/service/AnimalService.java
@@ -0,0 +1,8 @@
+package sa.site.lab.petstore.service;
+
+import sa.site.lab.petstore.domain.Animal;
+
+public interface AnimalService { //interface Driven design
+    public Animal findPet(int id);
+
+}
