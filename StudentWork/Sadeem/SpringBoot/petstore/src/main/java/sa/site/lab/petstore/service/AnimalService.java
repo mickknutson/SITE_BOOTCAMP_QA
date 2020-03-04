40ac419b4d17d22f33cd844fb1064f9061e85a33
Sadeem Alharbi <sadeemja@hotmail.com>
2020-03-04 23:29:38 +0300

    PetStore - Springboot Updated

--- StudentWork/Sadeem/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/service/AnimalService.java
+++ StudentWork/Sadeem/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/service/AnimalService.java
@@ -1,13 +1,15 @@
 package sa.site.lab.petstore.service;
 
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import sa.site.lab.petstore.domain.Animal;
 
 import java.util.List;
 
-
 public interface AnimalService {
-    public Animal findPet(int id);
+    public Animal findById(int id);
 
     List<Animal> findAll();
+    void add(Animal animal);
+
 }
