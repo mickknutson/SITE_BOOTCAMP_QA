9f59fbd73d4f3e05ac50efeadcb8440a36866c64
talal <tojasser@gmail.com>
2020-03-05 10:04:18 +0300

    adding proper Annotations

--- StudentWork/Talal/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/domain/Animal.java
+++ StudentWork/Talal/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/domain/Animal.java
@@ -1,11 +1,17 @@
 package sa.site.lab.petstore.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
 
 @Entity
 public class Animal {
     // primary key
+    @Id
     private int id ;
+
+    @Column
     private String name;
     private String type;
     private String sound;
@@ -18,15 +24,44 @@ public class Animal {
     }
 
     // TODO: Ignore these methods in JPA
-    public String getName() {
-        return name;
-    }
+    @Transient //ignore
     public void eat() {
        throw  new RuntimeException("Not yet implemented");
     }
-
+    @Transient 
     public void talk(String voice){
         throw new RuntimeException("Not yet implemented");
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
 }
