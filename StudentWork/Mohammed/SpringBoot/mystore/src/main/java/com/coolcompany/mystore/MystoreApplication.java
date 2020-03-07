9da559e3de7aa32bb9ca64df68ffb424f6a5d003
Mohammed <m7md21999@gmail.com>
2020-03-07 20:26:53 +0300

    re-added mystore project

---
+++ StudentWork/Mohammed/SpringBoot/mystore/src/main/java/com/coolcompany/mystore/MystoreApplication.java
@@ -0,0 +1,13 @@
+package com.coolcompany.mystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MystoreApplication.class, args);
+	}
+
+}
