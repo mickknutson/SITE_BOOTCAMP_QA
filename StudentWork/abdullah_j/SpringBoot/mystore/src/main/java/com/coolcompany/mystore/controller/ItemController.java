e04e4d21159da1765ba28871ee0819c3cdadcabd
Abdullah Alshehri <alshehri.a.j@gmail.com>
2020-03-07 16:07:06 +0300

    Implemented controller

--- StudentWork/abdullah_j/SpringBoot/mystore/src/main/java/com/coolcompany/mystore/controller/ItemController.java
+++ StudentWork/abdullah_j/SpringBoot/mystore/src/main/java/com/coolcompany/mystore/controller/ItemController.java
@@ -1,4 +1,25 @@
 package com.coolcompany.mystore.controller;
 
+import com.coolcompany.mystore.domain.Item;
+
+import java.util.List;
+
 public interface ItemController {
+
+    void add(Item item);
+
+    Item findById(int id);
+
+    List<Item> findAll();
+
+    void updateName(int id , String newName);
+
+    void updateDescription(int id , String newDescription);
+
+    void updateCost(int id , float newCost);
+
+    void deleteById(int id);
+
+    void deleteAll();
+
 }
