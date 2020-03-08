103c3ac6ad05ceb46b638606d25a513af894e23f
Lama <lama3skr@gmail.com>
2020-03-08 19:11:34 +0300

    Update

---
+++ StudentWork/Lama Alaskar/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/controller/AnimalControllerImpl.java
@@ -0,0 +1,33 @@
+package sa.site.lab.petstore.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import sa.site.lab.petstore.domain.Animal;
+import sa.site.lab.petstore.service.AnimalService;
+
+import java.util.List;
+
+@Controller
+public class AnimalControllerImpl implements AnimalController{
+
+    @Autowired
+    private AnimalService service;
+
+    @Override
+    public Animal findById(int id){
+        System.out.println("* AnimalController.findById: " + id);
+        return service.findById(id);
+    }
+
+    @Override
+    public List<Animal> findAll(){
+        System.out.println("* AnimalController.findAll()");
+        return service.findAll();
+    }
+
+    @Override
+    public void add(Animal animal){
+        System.out.println("* AnimalController.add()");
+        service.add(animal);
+    }
+}
---
+++ StudentWork/Lama Alaskar/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/dao/AnimalDao.java
@@ -0,0 +1,14 @@
+package sa.site.lab.petstore.dao;
+
+import sa.site.lab.petstore.domain.Animal;
+
+import java.util.List;
+
+public interface AnimalDao {
+
+    Animal findById(int id);
+
+    List<Animal> findAll();
+
+    void add(Animal animal);
+}
