0a900d9135e07d11de400b4f9d2d4e8ceb8a10ad
Hisham Bin Seddeq <prog.hbs@hotmail.com>
2020-03-08 14:49:21 +0300

    Thymeleaf Added and connected to controllers

--- StudentWork/Hisham/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/controller/AnimalControllerImpl.java
+++ StudentWork/Hisham/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/controller/AnimalControllerImpl.java
@@ -2,31 +2,49 @@ package sa.site.lab.petstore.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import sa.site.lab.petstore.domain.Animal;
 import sa.site.lab.petstore.service.AnimalService;
 
 import java.util.List;
 
+//@requestparam == queryString
+//@pathvariable ==
 
 @Controller
+@RequestMapping("/animals")
 public class AnimalControllerImpl implements AnimalController
 {
     @Autowired
     private AnimalService service;
 
+    @GetMapping("/{id}")
     @Override
-    public Animal findById(int id)
+    public String findById(@PathVariable int id, Model model)
     {
         System.out.println("* Animal Controller.FindById : "+id);
 
-        return service.findById(id);
+        Animal animal = service.findById(id);
+
+        model.addAttribute("animal",animal);
+
+        return "/animal";
     }
 
+    // accept requests on /list
+    @GetMapping("/list.html")
     @Override
-    public List<Animal> findAll()
+    public String findAll(Model model)
     {
         System.out.println("* Animal Controller.FindAll() : ");
-        return service.findAll();
+        List<Animal> allAnimals = service.findAll();
+        // add the animals object to the view
+        model.addAttribute("allAnimals",allAnimals);
+        return "/list";
     }
 
     @Override
