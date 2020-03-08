6dcddd5c451cf851b6c2ed4021e018ff03a8357c
Abdullah Alshehri <alshehri.a.j@gmail.com>
2020-03-08 14:29:21 +0300

    added html pages

--- StudentWork/abdullah_j/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/controller/AnimalControllerImpl.java
+++ StudentWork/abdullah_j/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/controller/AnimalControllerImpl.java
@@ -3,34 +3,49 @@ package sa.site.lab.petstore.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import sa.site.lab.petstore.domain.Animal;
 import sa.site.lab.petstore.service.AnimalService;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/animal")
 public class AnimalControllerImpl implements AnimalController {
 
 
     @Autowired
     private AnimalService service;
 
+    @GetMapping("/{id}")
     @Override
-    public Animal findById(int id) {
+    public String findById(@PathVariable int id , Model model) {
+
         System.out.println("*** AnimalController.FindPet(" + id + ") ***");
-        return service.findById(id);
+        Animal animal = service.findById(id);
+
+        model.addAttribute("animal" , animal);
+
+        return "animal";
     }
 
     // accept request on:
     //http://localhost:8080/list.html
-    @GetMapping("/list.html")
+    @GetMapping("/list.html") //should give us model
     @Override
-    public String findAll() {
+    public String findAll(Model model) {
         System.out.println("*** AnimalController.findAll() ***");
+
+        // Need model object
         List<Animal> allAnimals = service.findAll();
+
+        model.addAttribute("allAnimals" , allAnimals);
+
         return "List";
     }
 
