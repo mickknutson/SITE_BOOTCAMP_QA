efaf38ec070adf880efca13f2e5265a129c73d46
ghadeer <ghadeer.beniedar@gmail.com>
2020-03-08 15:25:16 +0300

    PetStore-added findById and findAll models&implementation

--- StudentWork/Ghadeer/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/controller/AnimalControllerImpl.java
+++ StudentWork/Ghadeer/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/controller/AnimalControllerImpl.java
@@ -2,8 +2,11 @@ package sa.site.lab.petstore.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import sa.site.lab.petstore.domain.Animal;
 import sa.site.lab.petstore.service.AnimalService;
 
@@ -11,14 +14,19 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-@RequestMapping("/")//url i want to map to
+@RequestMapping("/animal")//url i want to map to
 public class AnimalControllerImpl implements AnimalController {
     @Autowired
     private AnimalService service;
-    public Animal findById(int id){
+
+    @GetMapping("/{id}")
+    @Override
+    public String findById(@PathVariable int id, Model model){
         System.out.println("AnimalController.findPet:"+id);
 
-        return service.findById(id);
+        Animal animal= service.findById(id);
+        model.addAttribute("animal",animal);
+        return "Animal";//smth to view in animal.html
     } //End findPet method
 
     /*
@@ -28,15 +36,22 @@ public class AnimalControllerImpl implements AnimalController {
      */
     @GetMapping("/List.html")
     @Override
-    public String findAll(){
+    public String findAll(Model model){
         System.out.println("AnimalController.findAll()");
 
         List<Animal> allAnimals=service.findAll();
 
-        return "List";//send back
+        //add list to the response model
+        model.addAttribute("allAnimals",allAnimals);
+
+
+        return "List"; //send back the client page
     }
+    //add http mapping
+    @Override
     public void add(Animal animal){
         service.add(animal);
+        //todo: add proper logic
     }
 
 //    public void fillMethod(){
