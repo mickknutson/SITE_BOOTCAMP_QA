d3d4a5c2f4b38aca2b2969efb46f4fc32ddab877
talal <tojasser@gmail.com>
2020-03-08 15:23:06 +0300

    Added thymeleaf  files

--- StudentWork/Talal/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/controller/AnimalControllerImpl.java
+++ StudentWork/Talal/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/controller/AnimalControllerImpl.java
@@ -2,28 +2,53 @@ package sa.site.lab.petstore.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import sa.site.lab.petstore.domain.Animal;
 import sa.site.lab.petstore.service.AnimalService;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/animal")
 public class AnimalControllerImpl implements AnimalController {
 
     @Autowired
     private AnimalService animalService;
-
+    @GetMapping("/{id}")
     @Override
-    public Animal findById(int id) {
+    public String findById(@PathVariable int id,
+                           Model model) {
         System.out.println("* find By Id: "+id);
-        return animalService.findById(id);
+        Animal animal= animalService.findById(id);
+
+        model.addAttribute("animal", animal);
+        return "animal";
     }
 
+
+
+
+
+
+
+
+
+    @GetMapping("/list.html")
     @Override
-    public List<Animal> findAll() {
+    public String findAll(Model model) {
+
+        //Need model object
+        List <Animal> allanimals= animalService.findAll();
+        model.addAttribute("allAnimals",allanimals);
         System.out.println("Animal Controller: ");
-        return animalService.findAll();
+              //  animalService.findAll();
+        return "list";
     }
 
     @Override
