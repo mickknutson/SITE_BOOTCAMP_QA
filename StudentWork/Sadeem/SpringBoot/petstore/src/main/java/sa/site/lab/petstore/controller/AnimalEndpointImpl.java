b8864199f1592c23fce6ca07c3bc0b99433bbe7a
Sadeem Alharbi <sadeemja@hotmail.com>
2020-03-09 13:57:44 +0300

    update petstore_

--- StudentWork/Sadeem/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/controller/AnimalEndpointImpl.java
+++ StudentWork/Sadeem/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/controller/AnimalEndpointImpl.java
@@ -1,12 +1,10 @@
 package sa.site.lab.petstore.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sa.site.lab.petstore.domain.Animal;
 import sa.site.lab.petstore.service.AnimalService;
 
@@ -19,8 +17,10 @@ class AnimalEndpointImpl {
     private AnimalService service;
     //METHOD:
     @GetMapping("/{id}")
-    public String findById(@PathVariable int id, Model model){
-        return service.findById(id).toString();
+    @ResponseStatus(HttpStatus.ALREADY_REPORTED)
+    public Animal findById(@PathVariable int id, Model model){
+        // FIXME: Add validation
+        return service.findById(id);
     }
 
     @GetMapping
@@ -28,5 +28,16 @@ class AnimalEndpointImpl {
         List<Animal> animals = service.findAll();
         System.out.println("** REST Animals: " + animals);
         return animals;
+    }
+
+    //List<int> List<boolean< List<boolean>
+    public Boolean delete(int id){
+        throw new RuntimeException("Not implemented yet");
+    }
+    public Boolean add(Animal animal){
+        throw new RuntimeException("Not implemented yet");
+    }
+    public Boolean update(Animal animal){
+        throw new RuntimeException("Not implemented yet");
     }
 } // The End...
