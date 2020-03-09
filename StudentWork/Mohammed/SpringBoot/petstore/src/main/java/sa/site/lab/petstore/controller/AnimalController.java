e31fc7079ec7ad2ff0ec8a6a5ab3532936abdd0a
Mohammed <m7md21999@gmail.com>
2020-03-09 09:16:12 +0300

    Added add/create page

--- StudentWork/Mohammed/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/controller/AnimalController.java
+++ StudentWork/Mohammed/SpringBoot/petstore/src/main/java/sa/site/lab/petstore/controller/AnimalController.java
@@ -10,4 +10,6 @@ public interface AnimalController {
      String findById(int id, Model model);
     String findAll(Model model);
     String add(Model model);
+    String create (Animal animal);
+
 }
