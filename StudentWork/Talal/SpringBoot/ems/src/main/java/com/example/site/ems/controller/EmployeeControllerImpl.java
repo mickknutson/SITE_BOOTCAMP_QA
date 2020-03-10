6426b86fe0328cc76a2c5d02d74114beda82fd82
Mohammed <m7md21999@gmail.com>
2020-03-10 12:29:11 +0300

    commit

--- StudentWork/Talal/SpringBoot/ems/src/main/java/com/example/site/ems/controller/EmployeeControllerImpl.java
+++ StudentWork/Talal/SpringBoot/ems/src/main/java/com/example/site/ems/controller/EmployeeControllerImpl.java
@@ -40,7 +40,7 @@ public class EmployeeControllerImpl implements EmployeeController {
     @PostMapping("/create")
     public String create(Employee employee){
         employeeService.add(employee);
-        return "redirect://employee/employeesList";
+        return "redirect:/localhost8080:/employee/employeesList";
     }
 
     @PostMapping("/add")
