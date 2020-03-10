22a72d5aba57a8c1cff004323cd284b3c12060a3
talal <tojasser@gmail.com>
2020-03-10 09:56:13 +0300

    Added domain class

--- StudentWork/Talal/SpringBoot/ems/src/main/java/com/example/site/ems/domain/Employee.java
+++ StudentWork/Talal/SpringBoot/ems/src/main/java/com/example/site/ems/domain/Employee.java
@@ -1,4 +1,94 @@
 package com.example.site.ems.domain;
 
+import javax.persistence.*;
+import java.util.Date;
+@Entity
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id ;
+    @Column
+    private String firstName;
+    private String lastName;
+    private Date date;
+    private String email;
+    private String department;
+    private int salary;
+
+    public Employee(String firstName, String lastName, Date date, String email, String department, int salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.date = date;
+        this.email = email;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public Employee() {
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", date=" + date +
+                ", email='" + email + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 }
