5d0f157044203e4dcca54fc5b37f9471f5aec6d9
ghadeer <ghadeer.beniedar@gmail.com>
2020-03-10 10:54:28 +0300

    Dao package completed

--- StudentWork/Ghadeer/SpringBoot/ems/src/main/java/com/example/site/ems/dao/EmployeeDao.java
+++ StudentWork/Ghadeer/SpringBoot/ems/src/main/java/com/example/site/ems/dao/EmployeeDao.java
@@ -1,4 +1,19 @@
 package com.example.site.ems.dao;
 
+import com.example.site.ems.domain.Employee;
+
+import java.util.List;
+
 public interface EmployeeDao {
+
+    Employee findById(long id);
+
+    List<Employee> findAll();
+
+    boolean add(Employee employee);
+
+    boolean delete(long id);
+
+    boolean update(Employee updatedEmployee);
+
 }
