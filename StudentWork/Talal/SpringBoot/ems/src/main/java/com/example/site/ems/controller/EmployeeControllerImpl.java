5c89a7356182b05daa47032e7f24ce823b2b7855
Mohammed <m7md21999@gmail.com>
2020-03-10 13:18:09 +0300

    updates

--- StudentWork/Talal/SpringBoot/ems/src/main/java/com/example/site/ems/controller/EmployeeControllerImpl.java
+++ StudentWork/Talal/SpringBoot/ems/src/main/java/com/example/site/ems/controller/EmployeeControllerImpl.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-@RequestMapping("ems")
+@RequestMapping("/ems")
 public class EmployeeControllerImpl implements EmployeeController {
 
     @Autowired
