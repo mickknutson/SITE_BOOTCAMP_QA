e1fde804534f4f9fc306d38c34316884cd6f1009
Abdullah Alshehri <alshehri.a.j@gmail.com>
2020-03-11 15:36:37 +0300

    updated ems project

--- StudentWork/abdullah_j/SpringBoot/ems/src/main/resources/data.sql
+++ StudentWork/abdullah_j/SpringBoot/ems/src/main/resources/data.sql
@@ -1,13 +1,11 @@
 
-INSERT INTO employee(employee_Id,first_Name,last_Name,hire_Date,email,department,salary) VALUES (1535112,'Abdullah','Alshehri','2020-02-02','abdullah@site.com','QA',10000);
-
-insert into employee (employee_Id, first_Name, last_Name, hire_Date, email, department, salary) values (1, 'Giselbert', 'Housegoe', '2019-10-23', 'ghousegoe0@tuttocitta.it', 'amz', 25268.55);
-insert into employee (employee_Id, first_Name, last_Name, hire_Date, email, department, salary) values (2, 'Massimo', 'Howlett', '2020-01-04', 'mhowlett1@mashable.com', 'ybm', 10438.71);
-insert into employee (employee_Id, first_Name, last_Name, hire_Date, email, department, salary) values (3, 'Kyrstin', 'Leivesley', '2019-07-07', 'kleivesley2@imageshack.us', 'khu', 29423.47);
-insert into employee (employee_Id, first_Name, last_Name, hire_Date, email, department, salary) values (4, 'Matelda', 'Dillingstone', '2019-04-22', 'mdillingstone3@networkadvertising.org', 'ilz', 13418.08);
-insert into employee (employee_Id, first_Name, last_Name, hire_Date, email, department, salary) values (5, 'Jaclin', 'Coalbran', '2019-07-23', 'jcoalbran4@ehow.com', 'dpn', 14851.08);
-insert into employee (employee_Id, first_Name, last_Name, hire_Date, email, department, salary) values (6, 'Kandace', 'Iban', '2019-09-12', 'kiban5@ehow.com', 'pqq', 8889.84);
-insert into employee (employee_Id, first_Name, last_Name, hire_Date, email, department, salary) values (7, 'Nyssa', 'Cundy', '2019-11-21', 'ncundy6@addtoany.com', 'kjb', 12009.13);
-insert into employee (employee_Id, first_Name, last_Name, hire_Date, email, department, salary) values (8, 'Dorree', 'Dearle', '2019-09-06', 'ddearle7@arstechnica.com', 'crr', 15390.61);
-insert into employee (employee_Id, first_Name, last_Name, hire_Date, email, department, salary) values (9, 'Milka', 'Menci', '2019-10-15', 'mmenci8@unblog.fr', 'jzg', 9082.81);
-insert into employee (employee_Id, first_Name, last_Name, hire_Date, email, department, salary) values (10, 'Vinnie', 'Torbeck', '2019-09-29', 'vtorbeck9@stumbleupon.com', 'fjf', 24039.22);
+insert into employee (employee_Id, first_Name, last_Name, hire_Date, email, department, salary) values (1, 'Giselbert', 'Housegoe', '2019-10-23', 'ghousegoe0@tuttocitta.it', 'HR', 25268.55);
+insert into employee (employee_Id, first_Name, last_Name, hire_Date, email, department, salary) values (2, 'Massimo', 'Howlett', '2020-01-04', 'mhowlett1@mashable.com', 'IT', 10438.71);
+insert into employee (employee_Id, first_Name, last_Name, hire_Date, email, department, salary) values (3, 'Kyrstin', 'Leivesley', '2019-07-07', 'kleivesley2@imageshack.us', 'HR', 29423.47);
+insert into employee (employee_Id, first_Name, last_Name, hire_Date, email, department, salary) values (4, 'Matelda', 'Dillingstone', '2019-04-22', 'mdillingstone3@networkadvertising.org', 'Finance', 13418.08);
+insert into employee (employee_Id, first_Name, last_Name, hire_Date, email, department, salary) values (5, 'Jaclin', 'Coalbran', '2019-07-23', 'jcoalbran4@ehow.com', 'Management', 14851.08);
+insert into employee (employee_Id, first_Name, last_Name, hire_Date, email, department, salary) values (6, 'Kandace', 'Iban', '2019-09-12', 'kiban5@ehow.com', 'QA', 8889.84);
+insert into employee (employee_Id, first_Name, last_Name, hire_Date, email, department, salary) values (7, 'Nyssa', 'Cundy', '2019-11-21', 'ncundy6@addtoany.com', 'IT', 12009.13);
+insert into employee (employee_Id, first_Name, last_Name, hire_Date, email, department, salary) values (8, 'Dorree', 'Dearle', '2019-09-06', 'ddearle7@arstechnica.com', 'QA', 15390.61);
+insert into employee (employee_Id, first_Name, last_Name, hire_Date, email, department, salary) values (9, 'Milka', 'Menci', '2019-10-15', 'mmenci8@unblog.fr', 'Management', 9082.81);
+insert into employee (employee_Id, first_Name, last_Name, hire_Date, email, department, salary) values (10, 'Vinnie', 'Torbeck', '2019-09-29', 'vtorbeck9@stumbleupon.com', 'QA', 24039.22);
