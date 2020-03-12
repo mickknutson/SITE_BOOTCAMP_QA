bafbbdeb3dc31261ffd872f3ef70270679651b04
Nader <na511dr@hotmail.com>
2020-03-12 09:58:40 +0300

    update theme (ems)

--- StudentWork/Nader/project/ems/src/main/java/com/example/site/ems/controller/EmployeeController.java
+++ StudentWork/Nader/project/ems/src/main/java/com/example/site/ems/controller/EmployeeController.java
@@ -2,8 +2,18 @@ package com.example.site.ems.controller;
 
 import com.example.site.ems.domain.Employee;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 
 public interface EmployeeController {
 
+    String gotoAdd(Employee employee);
+    String listAll(Model model);
+    String add(Employee employee, BindingResult result);
+    String gotoUpdate(int id, Model model);
+    String update(int id,
+                         Employee employee, BindingResult result,
+                         Model model);
+    public String delete(int id, Model model);
 
-}
+    }
