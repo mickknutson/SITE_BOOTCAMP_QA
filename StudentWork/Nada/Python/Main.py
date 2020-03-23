d7a38e74bafb425b9680989f503bdd12fba0bcf9
Nada <nhalghani@gmail.com>
2020-03-23 18:00:06 +0300

    String exercise solution

--- StudentWork/Nada/Python/Main.py
+++ StudentWork/Nada/Python/Main.py
@@ -5,14 +5,5 @@ else:
    print("{0} is Odd number".format(num))
 
 
-def name(s):
-   # split the string into a list 
-   l = s.split ()
-   new = ""
-   for i in range ( len ( l ) - 1 ):
-      s = l[i]
-   new += l[-1].title ()
-   return new
-
 s = "Nada Aljohani"
-print ( name ( s ) )
+print (s[4:12]  )
