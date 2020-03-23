17b4020fc401943821296e2f11ef783786dd3b80
ghadeer <ghadeer.beniedar@gmail.com>
2020-03-23 18:10:46 +0300

    added string exercise

--- StudentWork/Ghadeer/Python/Main.py
+++ StudentWork/Ghadeer/Python/Main.py
@@ -10,3 +10,12 @@ def evenOdd( x ):
 evenOdd(2)
 
 evenOdd(3)
+# a use of raw_input()
+
+name = input("Enter your full name: ")
+#in this way i used finding the index of the seperation character and then substring starting from
+#the defind index+1 to not include the seperation character with the surname
+index=1+name.index(' ',2)
+print (name[(index):]);
+#in this way i used pre-defind method in python
+print (name.split()[1]);
