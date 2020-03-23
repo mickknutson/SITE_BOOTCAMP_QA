bf8f330f0c39850f2c509d1118bbd5b297e547d9
Lama <lamafa97@gmail.com>
2020-03-23 18:34:57 +0300

    loop exercise added

--- StudentWork/Lama Alyousef/Python/Main.py
+++ StudentWork/Lama Alyousef/Python/Main.py
@@ -1,3 +1,5 @@
+# Even and Odd Function
+
 def evenOdd(x):
     if (x% 2 == 0):
         print("even");
@@ -7,3 +9,8 @@ def evenOdd(x):
 evenOdd(2)
 evenOdd(3)
 
+# loop of multiplication table of 19
+
+num = 19
+for i in range(1,11):
+    print (num, 'x', i, "=", num * i)
