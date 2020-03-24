267052991325896e87a81b2e24dec909c0206703
Sadeem Alharbi <sadeemja@hotmail.com>
2020-03-24 17:34:22 +0300

    Edit RegEx solution

--- StudentWork/Sadeem/Python/Regex.py
+++ StudentWork/Sadeem/Python/Regex.py
@@ -1,8 +1,8 @@
 import re as regex
 
-number = "966503317351"
+number = "+966503317351"
 
-test_number = "^(9665)[0-9]{8}"
+test_number = "^(\+9665)[0-9]{8}"
 
 result = regex.match(test_number, number)
 
