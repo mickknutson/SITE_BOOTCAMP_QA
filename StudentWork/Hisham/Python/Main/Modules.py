52d446fb517caeeba3bd8545375022a5d6660339
Hisham Bin Seddeq <prog.hbs@hotmail.com>
2020-03-24 16:58:00 +0300

    Added and finished Regex Exercise

--- StudentWork/Hisham/Python/Main/Modules.py
+++ StudentWork/Hisham/Python/Main/Modules.py
@@ -7,16 +7,28 @@ print("Math module printing the pi value = {}".format(math.pi))
 
 ## RegEx
 
-import re
+import re as regex
 
 pattern = "^[hH]....m$"
 test_string_cap = "Hisham"
 test_string_small = "hisham"
-result = re.match(pattern,test_string_small)
-result2 = re.match(pattern,test_string_cap)
+result = regex.match(pattern, test_string_small)
+result2 = regex.match(pattern, test_string_cap)
 
 
+testing_saudi_number = "+966567057050"
+number_pattern = "^[+966]+\d{9}$"
+result3 = regex.match(number_pattern, testing_saudi_number)
+
+
+## letters matching
 if result and result2:
     print("Match")
+else:
+    print("No Match")
+
+## numbers matching
+if result3:
+    print("Match Saudi Number")
 else:
     print("No Match")
